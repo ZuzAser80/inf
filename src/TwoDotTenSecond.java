1fa72428aba850fb0bc06ffdf6cbf62774b15a07
Danil <dde4th@gmail.com>
2020-11-07 15:09:38 +0500

    Endgame

--- src/TwoDotTenSecond.java
+++ src/TwoDotTenSecond.java
@@ -7,35 +7,20 @@ import java.util.Scanner;
 2.10.1.5
 Составить программу, которая при введенном возрасте человека правильно печатает: «Вам х лет (год, года)!».
  */
-/*
-
-10 <
-1 - год
-2, 3, 4 - года
-5, 6, 7, 8, 9, 10 - лет
-
-11 - 20  - лет
-
-20 >
-X1 - год
-X2, X3 , X4 - года
-X5, X6, X7, X8, X9, (X+1)0 - лет
-
- */
 public class TwoDotTenSecond
 {
     public static void main(String[] args)
     {
         System.out.println("Введите число");
-        for (int i = 1;i < 100;i++) {
-            System.out.println("Вам " + i + " " + printAges(i));
-        }
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println("Вам " + printAges(i));
     }
 
     static String printAges(int age) {
         if (age >= 10 && age <= 20)
         {
-            return "год";
+            return "лет";
         }
         if (age == 1)
         {
